c1ce05827f5d3d17053e87bdb24a8514c66216e2
Marcelo Salloum <marcelo@stellar.org>
2021-03-10 16:37:45 -0300

    Create input CLI option that configures the SEP-8 asset issuer.

---
+++ services/regulated-assets-approval-server/cmd/configure-asset-issuer.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"fmt"
+	"go/types"
+
+	"github.com/spf13/cobra"
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/support/config"
+)
+
+type AssetIssuerOptions struct {
+	HorizonURL          string
+	NetworkPassphrase   string
+	AccountIssuerSecret string
+}
+
+type ConfigureAssetIssuer struct{}
+
+func (c *ConfigureAssetIssuer) Command() *cobra.Command {
+	opts := AssetIssuerOptions{}
+	configOpts := config.ConfigOptions{
+		{
+			Name:      "account-issuer-secret",
+			Usage:     "Secret key of the asset issuer's stellar account.",
+			OptType:   types.String,
+			ConfigKey: &opts.AccountIssuerSecret,
+			Required:  true,
+		},
+		{
+			Name:        "horizon-url",
+			Usage:       "Horizon URL used for looking up account details",
+			OptType:     types.String,
+			ConfigKey:   &opts.HorizonURL,
+			FlagDefault: horizonclient.DefaultTestNetClient.HorizonURL,
+			Required:    true,
+		},
+		{
+			Name:        "network-passphrase",
+			Usage:       "Network passphrase of the Stellar network transactions should be signed for",
+			OptType:     types.String,
+			ConfigKey:   &opts.NetworkPassphrase,
+			FlagDefault: network.TestNetworkPassphrase,
+			Required:    true,
+		},
+	}
+	cmd := &cobra.Command{
+		Use:   "configure-asset-issuer",
+		Short: "Configure asset issuer to use SEP-8 regulated assets.",
+		Run: func(_ *cobra.Command, _ []string) {
+			configOpts.Require()
+			configOpts.SetValues()
+			c.Run(opts)
+		},
+	}
+	configOpts.Init(cmd)
+	return cmd
+}
+
+func (c *ConfigureAssetIssuer) Run(opts AssetIssuerOptions) {
+	fmt.Printf("tada! %+v\n", opts)
+}
--- services/regulated-assets-approval-server/main.go
+++ services/regulated-assets-approval-server/main.go
@@ -20,6 +20,7 @@ func main() {
 	}
 
 	rootCmd.AddCommand((&cmd.ServeCommand{}).Command())
+	rootCmd.AddCommand((&cmd.ConfigureAssetIssuer{}).Command())
 
 	err := rootCmd.Execute()
 	if err != nil {
